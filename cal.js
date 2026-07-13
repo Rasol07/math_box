6da940e2717282ac1761e1594eec800caf7d9d89
Rasol07 <poloa0709@gmail.com>
2026-07-13 15:32:03 +0900

    Refactor: calculator.js

--- cal.js
+++ cal.js
@@ -1,3 +1,9 @@
-const add = (a, b) => (a = b);
-const abstract = (a, b) => a - b;
-const multiply = (a, b) => a * b;
+const add = (a, b) => {
+  return a + b;
+};
+const abstract = (a, b) => {
+  return a - b;
+};
+const multiply = (a, b) => {
+  return a * b;
+};
