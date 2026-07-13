71b48a446a53a4017dcaa8ace1c62e87357367a4
Rasol07 <poloa0709@gmail.com>
2026-07-13 15:23:06 +0900

    Update multiply function
    calculator.js supports mulitply now.

--- cal.js
+++ cal.js
@@ -1,8 +1,3 @@
-function add(a, b) {
-  return a + b;
-}
-
-// 빼기 - 수정용
-function substract(a, b) {
-  return a - b;
-}
+const add = (a, b) => (a = b);
+const abstract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
