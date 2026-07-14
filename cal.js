f951b27c7737a32990dd6961708c8e3e5da7a62e
Rasol07 <poloa0709@gmail.com>
2026-07-14 11:23:29 +0900

    Add comments

--- cal.js
+++ cal.js
@@ -1,13 +1,19 @@
+// 덧셈
 const add = (a, b) => {
   return a + b;
 };
+
+//뺄셈
 const abstract = (a, b) => {
   return a - b;
 };
+
+//곱셈
 const multiply = (a, b) => {
   return a * b;
 };
-// dtd
+
+//나눗셈
 const divide = (a, b) => {
   return a / b;
 };
